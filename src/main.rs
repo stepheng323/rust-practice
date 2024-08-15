9bf5efc1d7f5d8c0fe8a6418772a72081512885f
Abiodun <stepheng323@gmail.com>
2024-08-15 21:55:15 +0100

    use-try

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 use std::fs;
+use std::io::Error;
 
-fn main() {
-    let mut error_logs = vec![];
-    match fs::read_to_string("logs.txt") {
-        Ok(value) => {
-            error_logs = extract_errors(value.as_str());
-        }
-        Err(error) => {
-            println!("An error occurred {}", error);
-        }
-    }
-    println!("{:#?}", error_logs)
+fn main() -> Result<(), Error>{
+
+    // let text = fs::read_to_string("logs.txt").expect("failed to read log file");
+
+    // let error_logs = extract_errors(text.as_str());
+
+    // fs::write("error.txt", error_logs.join("\n")).expect("failed to write error to file")
+
+
+    let text = fs::read_to_string("logs.txt")?;
+
+    let error_logs = extract_errors(text.as_str());
+
+    fs::write("error.txt", error_logs.join("\n"))?;
+
+    Ok(())
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(value) => {
+    //         let error_logs = extract_errors(value.as_str());
+    //         match fs::write("error.txt", error_logs.join("\n")) {
+    //             Ok(..)=> println!("error written successfully"),
+    //             Err(err) => println!("Error writing text to file {}", err)
+    //         }
+    //     }
+    //     Err(error) => {
+    //         println!("An error occurred {}", error);
+    //     }
+    // }
 }
-
-
-fn extract_errors(text: &str) -> Vec<&str> {
+fn extract_errors(text: &str) -> Vec<String> {
     let split_text = text.split("\n");
     let mut result = vec![];
     for line in split_text {
         if line.starts_with("ERROR") {
-            result.push(line);
+            result.push(line.to_string());
         }
     }
     result
